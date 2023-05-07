ec0dd153d1ec561698092eb8b547e525624faa53
Alexander Hill <ahill@breadpudding.dev>
2023-05-07 11:48:05 -0400

    Fixed a rendering bug on my laptop

--- src/system/unix-glx/interface.c
+++ src/system/unix-glx/interface.c
@@ -50,6 +50,7 @@ int main(int argc, char *argv[]) {
             case Expose:
                 XGetWindowAttributes(display, window, &winattr);
                 glViewport(0, 0, winattr.width, winattr.height);
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
                 glBegin(GL_TRIANGLES);
                 glColor3f(1.0, 0.0, 0.0);
                 glVertex2f(-0.5, -0.5);
