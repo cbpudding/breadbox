9e39c8fb03b3be44a083082f66b28b87693366d7
Alexander Hill <ahill@breadpudding.dev>
2023-05-12 22:40:01 -0400

    The triangle now spins!

--- include/breadbox.h
+++ include/breadbox.h
@@ -5,12 +5,6 @@
 
 #define BREADBOX_TICKRATE 20
 
-typedef struct {
-    // Current game tick
-    int tick;
-} breadbox_model_t;
-
-// This is how the system layer sends events to the engine layer. ~Alex
 typedef enum {
     // Tell the engine that a tick has passed
     BBMSG_TICK,
@@ -18,21 +12,31 @@ typedef enum {
     BBMSG_VIEW
 } breadbox_message_t;
 
-// This function should render the given model so the player can see the
-// current state of the game. ~Alex
-void breadbox_view(breadbox_model_t *model);
+typedef struct {
+    // Current game tick
+    int tick;
+} breadbox_model_t;
 
 typedef struct {
     // ...
 } breadbox_subscriptions_t;
-
-void breadbox_update(breadbox_model_t *model, breadbox_message_t *msg);
 
 typedef struct {
     breadbox_model_t model;
     breadbox_subscriptions_t subscriptions;
 } breadbox_t;
 
+// This is the main way the engine is notified of events by the system. ~Alex
 void breadbox_publish(breadbox_t *engine, breadbox_message_t *msg);
+
+// This function is how we tell the game what's going on. This should only
+// be called by the subscription module or messages might not be routed
+// properly. Unless this is called from the subscription service, you should
+// probably be using breadbox_publish instead. ~Alex
+void breadbox_update(breadbox_model_t *model, breadbox_message_t *msg);
+
+// This function should render the given model so the player can see the
+// current state of the game. ~Alex
+void breadbox_view(breadbox_model_t *model);
 
 #endif
--- src/system/unix-glx/view.c
+++ src/system/unix-glx/view.c
@@ -8,8 +8,14 @@ extern Display *DISPLAY;
 extern Window WINDOW;
 
 void breadbox_view(breadbox_model_t *model) {
+    float angle;
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     // Test triangle to be replaced by actual rendering code later. ~Alex
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    // bananas ROTAT E ~Alex
+    angle = (float)(model->tick % (BREADBOX_TICKRATE * 4)) / ((float)BREADBOX_TICKRATE * 4);
+    glRotatef(angle * 360.0, 0.0, 1.0, 0.0);
     glBegin(GL_TRIANGLES);
     glColor3f(1.0, 0.0, 0.0);
     glVertex2f(-0.5, -0.5);
