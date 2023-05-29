fa3962c4e8c0d66b008a5e9d61da825ed10a0003
Alexander Hill <ahill@breadpudding.dev>
2023-05-28 23:34:48 -0400

    Decided to use a different header for files

--- include/resources.h
+++ include/resources.h
@@ -2,22 +2,43 @@
 #define RESOURCES_H
 
 typedef enum {
+    // Whatever the data is, it's not tied to a specific platform! ~Alex
+    BBPLAT_NEUTRAL,
+    // Data with this descriptor can only be used with the unix-glx target ~Alex
+    BBPLAT_UNIX_GLX
+} breadbox_resource_platform_t;
+
+typedef enum {
     // We don't know what type this is supposed to be and it's up to the game to
     // figure it out. ~Alex
     BBRES_UNKNOWN
 } breadbox_resource_type_t;
 
-// Assuming each field is only 32 bits, this structure should only be 16 bytes.
-// Maybe 20 if we add a signature like "BBR\0"... ~Alex
 typedef struct {
     // Type of resource
     breadbox_resource_type_t type;
     // Size of the resource in bytes
     int size;
-    // CRC32C Hash to verify resource integrity
+    // CRC32C hash to verify resource integrity
     int hash;
-    // Pointer or offset from the start of the file to the resource data
+    // Pointer to the resource data
     void *data;
 } breadbox_resource_t;
+
+typedef struct {
+    // File signature. Should always be "BBR\0". Maybe we could use this value
+    // to detect endianness or maybe even a version number sometime in the
+    // future? If "BBR\0" is 0x42425200 on little endian architectures, then we
+    // will read it as 0x00524242 if the file's endianness isn't correct! ~Alex
+    char signature[4];
+    // Supported platform
+    breadbox_resource_platform_t platform;
+    // Type of resource
+    breadbox_resource_type_t type;
+    // Size of the resource of bytes
+    int size;
+    // CRC32C hash to verify resource integrity
+    int hash;
+} breadbox_resource_header_t;
 
 #endif
